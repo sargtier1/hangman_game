21befd5f1cc4c5520f86694d1eb89249e79923ff
JustTams <sarg.tier1@gmail.com>
2018-09-06 15:44:10 -0500

    this is the best working verison so far.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 //////  ARRAY WITH POSSIBLE WORDS TO GUESS FROM //////
+
 var wordPool = 
   [
     "jacket", 
@@ -33,27 +34,24 @@ var guessLeft = document.getElementById("remaining");
 var wins = 0;
 var maxGuesses = 9;
 
-//Selects random word and creats empty array (bonus player guess var)
+//Selects random word and creats empty array
 var chosenWord = wordPool[Math.floor(Math.random() * wordPool.length)];
 var answer = [];
 
-// figures out how many characters are in selected word, updates game screen with stats //
-window.onload = function load() {
+window.onload = function () {
    
-    // figures out how many characters are in selected word, updates game screen with stats //
+    // figures out how many characters are in selected word //
     for (var i = 0; i < chosenWord.length; i++) {
-        answer[i] = "_";
-        console.log([i]);
-        console.log(answer)
-        console.log(chosenWord); //test
+        console.log(i);
+        answer.push("_");
+        console.log(answer);
+        console.log(chosenWord); 
     }
 
-    var remainingLetters = chosenWord.length
     document.getElementById("playArea").innerHTML = answer.join(" "); //replaces spaces with blanks on web page
     document.getElementById("remaining").innerHTML = maxGuesses; //puts max guesses on screen
     document.getElementById("wins").innerHTML = wins; // puts wins on screen
 
-  };
 
   //Runs everytime a key is pressed\\
 
@@ -61,48 +59,33 @@ document.onkeyup = function whenGuessed (event) {
 
     var playerGuess = (event.key);//figures out whats being pressed
     lettersGuessed.innerHTML += " " + playerGuess; //Players guess displayed
-    maxGuesses -1;
-    var remainingLetters = chosenWord.length; 
-
-      for (var j = 0; j < chosenWord.length; j++) {//this helps following code reference the hidden word
-        
-        if (playerGuess < "a" || playerGuess > "z") {
-          return;
-          }
-
-        if (chosenWord.charAt[j] === playerGuess) {
-          //change vale of answer[i] to current character
-          answer[j] = playerGuess;
-          remainingLetters - 1;
-          console.log(answer);
-          return;
-          }
-
-        if (chosenWord.charAt[j] !== playerGuess) {
-          // reduce guesses by 1
-          guessLeft = maxGuesses - 1;   
-          return;       
-          }
+    maxGuesses - 1;//removes guesses remaining by one each guess
+    guessLeft = maxGuesses
+    document.getElementById("remaining").innerHTML = guessLeft;
+    var remainingWords = answer.length // sees how many words are left blank
+    
+    
+      for (var i = 0; i < chosenWord.length; i++) {//this helps following code reference the hidden word
+      
+        if (chosenWord[i] === playerGuess) {
+          var remainingWords = answer.length
+          answer[i]= playerGuess; // checks if guess is the same as a character 
+          remainingWords -1; // reduce remaining words
+          console.log(answer); // console test
+          console.log(remainingWords);
+          document.getElementById("playArea").innerHTML = answer // displays new array with filled in blank spaces
+        }
 
         if (maxGuesses === 0) {
-          // end game
-            alert("You Lose!");
-            prompt("play Again?")
-            if (true) {
-              // execute load function
-            }
-          } 
+          // after 9 presses alerts "you lose!"
+            alert("You Lose");
         }
 
-        if (remainingLetters === 0){
-          alert("You Win!")
-          prompt("play Again?") 
-            if (true) {
-              //execute load funtion
-            }
+        if (remainingWords = 0) {
+          // if all spaces are filled you alert "you win!""
+          wins++;
         }
-        
+      }
+    
+  };
 };
-  // updating the playingfield
-
-
